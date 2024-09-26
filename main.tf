08bfabbee88e0a71a726a43395e1c9a62b1bd937
mamunoz <mamunoz@atsistemas.com>
2024-09-26 17:01:49 +0200

    modulo ec2

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "ec2_instance" {
+  count = var.environment == "dev" ? 1 : length(var.subnet_ids) * var.instances_per_subnet
+  # ami           = "ami-0ebfd941bbafe70c6" #AZ2023 en us-east-1
+  ami           = var.ami
+  instance_type = "t2.micro"
+  # subnet_id     = var.environment == "dev" ? var.subnet_ids[0] : element(var.subnet_ids, floor(count.index / var.instances_per_subnet))
+  subnet_id     = var.environment == "dev" ? var.subnet_ids[0] : var.subnet_ids[floor(count.index / var.instances_per_subnet)]
+  
+  tags = {
+    Name = "${var.instance_name_prefix}-instance-miguel-${count.index}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  description = "Environment"
+  type        = string
+  validation {
+    condition     = can(regex("(dev|prod)", var.environment))
+    error_message = "Environment must be dev or prod."
+  }
+}
+variable "subnet_ids" {
+  description = "List of subnet IDs where instances will be deployed"
+  type        = list(string)
+}
+variable "instances_per_subnet" {
+  description = "Number of instances to deploy per subnet"
+  type        = number
+  default     = 1
+}
+variable "ami" {
+  type = string
+  description = "AMI to deploy"
+}
+variable "instance_name_prefix" {
+  type = string
+  description = "Instance name prefix"
+}
